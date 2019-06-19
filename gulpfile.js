5f5252820bb283974653083fdae8eeee28e933cb
ArkadyKid <kidanov.a.y1986@gmail.com>
2019-06-19 18:44:18 +0300

    Подключил минификатор css

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 var gulp        = require('gulp'),
     sass        = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    cssNano     = require('gulp-cssnano'),
+    rename      = require('gulp-rename');
 
-gulp.task('sass', function(){
+gulp.task('sass', function() {
     return gulp.src('app/sass/**/*.{sass,scss}')
-        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.reload({stream: true}))
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}))
 });
+
+gulp.task('cssMin', function() {
+    return gulp.src('app/css/**/*.css')
+    .pipe(cssNano())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/css'));
+})
 
 gulp.task('browser-sync', function() {
     browserSync({
@@ -18,8 +27,20 @@ gulp.task('browser-sync', function() {
     })
 });
 
-gulp.task('watch', function() {
-    gulp.watch('app/sass/**/*.scss', gulp.parallel('sass'));
+gulp.task('scripts', function() {
+    return gulp.src(['app/js/common.js', 'app/libs/**/*.js'])
+    .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('default',  gulp.parallel('browser-sync','sass','watch'));
+gulp.task('code', function() {
+    return gulp.src('app/*.html')
+    .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('watch', function() {
+    gulp.watch('app/sass/**/*.sass', gulp.parallel('sass'));
+    gulp.watch('app/*.html', gulp.parallel('code'));
+    gulp.watch(['app/js/common.js', 'app/libs/**/*.js'], gulp.parallel('scripts'));
+});
+
+gulp.task('default', gulp.parallel('sass','browser-sync','watch'));
