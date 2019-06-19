d9ddde7f2f4435f077533b757db6d2269798e093
ArkadyKid <kidanov.a.y1986@gmail.com>
2019-06-19 15:46:55 +0300

    Поменял структуру

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 var gulp        = require('gulp'),
-    sass        = require('gulp-sass');
+    sass        = require('gulp-sass'),
+    browserSync = require('browser-sync');
 
 gulp.task('sass', function(){
-    return gulp.src('sass/**/*.{sass,scss}')
+    return gulp.src('app/sass/**/*.{sass,scss}')
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        notify: false
+    })
 });
 
 gulp.task('watch', function() {
-    gulp.watch('sass/**/*.{sass,scss}' , gulp.parallel('sass'));
+    gulp.watch('app/sass/**/*.scss', gulp.parallel('sass'));
 });
 
-gulp.task('default', gulp.parallel('watch'))
+gulp.task('default',  gulp.parallel('browser-sync','sass','watch'));
